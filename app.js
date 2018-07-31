550aa32ec449f2c9e362ecb9ffe18c76158381f2
Heng Xiao <hengxiao@hengxiao.net>
2018-07-31 01:33:16 -0700

    RSVP handler implemented. File in /home/heng890331/wedding_record/rsvp.txt\nWebpage style improved.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -23,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/index.html', indexRouter);
 app.use('/rsvp', rsvpRouter);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var os = require('os');
+var record_file = fs.createWriteStream("/home/heng890331/wedding_record/rsvp.txt", {flags: 'a', encoding: "utf8", mode: 0666});
+//var record_file = fs.createWriteStream(os.homedir() + "/wedding_record/rsvp.txt", {flags: 'a', encoding: "utf8", mode: 0666});
 
 /* GET RSVP page. */
 var rsvppage = function(req, res, next) {
@@ -8,16 +12,21 @@ var rsvppage = function(req, res, next) {
 
 /* POST RSVP page. */
 var rsvppost = function(req, res, next) {
-    var name = req.query.name;
-    var attending = req.query.attending;
-    var meal1 = req.query.meal1;
-    var meal2 = req.query.meal2;
-    console.log("name: " + name + ", attending: " + attending +
-		", meal1: " + meal1 + ", meal2: " + meal2);
+    console.log(res);
+    var name = req.body.name;
+    var attending = req.body.attending;
+    var turf = req.body.turf;
+    var surf = req.body.surf;
+    var vege = req.body.vege;
+    try {
+	record_file.write("name: " + name + ", attending: " + attending +
+			  ", turf: " + turf + ", surf: " + surf + ", vege: " + vege + "\n");
+    } catch (error) {
+	console.log("error");
+    }
     res.render('index');
 }
 
-router.get('/', rsvppost);
 router.post('/', rsvppost);
 
 module.exports = router;
