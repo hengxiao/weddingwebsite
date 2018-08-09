8f1111f16df4deb57851182bd769f631eda919c2
Heng Xiao <hengxiao@hengxiao.net>
2018-08-09 02:01:50 -0700

    1. Add our story 2. Finish gift registry and link 3. Enable RSVP, link from RSVP to dinner menu 4. Fix poptrox bug

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -300,3 +300,39 @@ jQuery(function() {
     });
 
 });
+
+$(document).ready(function() {
+    var showChar = 100;
+    var ellipsestext = "...";
+    var moretext = "&nbsp;more&nbsp;";
+    var lesstext = "&nbsp;less&nbsp;";
+    $('.more').each(function() {
+	var content = $(this).text();
+	if(content.length > showChar) {
+	    var c = content.substr(0, showChar);
+	    var h = content.substr(showChar, content.length - showChar);
+	    var html = c
+		+ '<span class="moreelipses">'
+		+ ellipsestext
+		+ '</span><span class="morecontent"><span>'
+		+ h
+		+ '</span><a href="" class="morelink">'
+		+ moretext
+		+ '</a></span>';
+	    $(this).html(html);
+	}
+    });
+
+    $(".morelink").click(function(){
+	if($(this).hasClass("less")) {
+	    $(this).removeClass("less");
+	    $(this).html(moretext);
+	} else {
+	    $(this).addClass("less");
+	    $(this).html(lesstext);
+	}
+	$(this).parent().prev().toggle();
+	$(this).prev().toggle();
+	return false;
+    });
+});
